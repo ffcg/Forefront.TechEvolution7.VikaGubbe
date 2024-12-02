5ac2434fb054f259112bc5527e28833525c71be7
Mahmoud Daabas <mahmoud.daabas@verisure.com>
2024-12-02 15:30:02 +0100

    init monkeys

--- VikaGubbeGustav/VikaGubbeGustav/Program.cs
+++ VikaGubbeGustav/VikaGubbeGustav/Program.cs
@@ -9,6 +9,37 @@ class Program
 
         //initialize the monkeys and give them the starting items
         //didnt understand assignment very well, sorry hehe
+        var monkeys = new List<Monkey>
+        {
+            new Monkey(
+                new List<int> { 79, 98 },
+                worryLevel => worryLevel * 19,
+                worryLevel => worryLevel % 23 == 0,
+                2,
+                3 
+            ),
+            new Monkey(
+                new List<int> { 54, 65, 75, 74 },
+                worryLevel => worryLevel + 6, 
+                worryLevel => worryLevel % 19 == 0, 
+                2, 
+                0  
+            ),
+            new Monkey(
+                new List<int> { 79, 60, 97 },
+                worryLevel => worryLevel * worryLevel, 
+                worryLevel => worryLevel % 13 == 0, 
+                1,
+                3 
+            ),
+            new Monkey(
+                new List<int> { 74 },
+                worryLevel => worryLevel + 3,
+                worryLevel => worryLevel % 17 == 0, 
+                0, 
+                1  
+            )
+        };
 
     }
 }
@@ -16,14 +47,18 @@ class Program
 class Monkey
 {
     public List<int> StartingItems { get; set; }
-    public bool Operation { get; set; } // If true then use multiplication in the operation, if false then use plus, also feel free to rename
+    public Func<int, int> Operation { get; set; }
     public Func<int, bool> Test { get; set; }
+    public int TrueMonkey { get; set; }
+    public int FalseMonkey { get; set; } 
 
-    public Monkey(List<int> startingItems, bool operation, Func<int, bool> test)
+    public Monkey(List<int> startingItems, Func<int, int> operation, Func<int, bool> test, int trueMonkey, int falseMonkey)
     {
         StartingItems = startingItems;
         Operation = operation;
         Test = test;
+        TrueMonkey = trueMonkey;
+        FalseMonkey = falseMonkey;
     }
 
     public void InspectItems()
