e8ec8de9028aff25aa13a6b7c26d9e4157b09927
Gustav Franchell <gfranchell@gmail.com>
2024-12-02 14:53:17 +0100

    Day 5 first commit

---
+++ VikaGubbeGustav/VikaGubbeGustav/Day5.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Day5
+{
+    public static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("input.txt");
+
+        string[] seedStrings = lines[0].Split(": ")[1].Split(' ');
+        List<long> seeds = seedStrings.Select(long.Parse).ToList();
+    }
+
+}
