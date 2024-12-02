e2d0c6e2832d5f998f9fe0263afa5a53fecaa040
zeuph <kimmie.arvidsson@forefront.se>
2024-12-02 14:31:48 +0100

    initial commit with random generator of cards

---
+++ AoC2307/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        string[] possibleCards = { "A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2" };
+
+        GetAHandOfRandomCards(possibleCards, 5);
+    }
+
+    static string[] GetAHandOfRandomCards(string[] deck, int count)
+    {
+        Random random = new Random();
+        string[] selectedCards = new string[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            int randomIndex = random.Next(deck.Length); // Generate a random index
+            selectedCards[i] = deck[randomIndex]; // Select the card
+        }
+
+        return selectedCards;
+    }
+
+    // Function to look at a hand and output its "type" according to the advent of code assignment
+
+    // Function to compare "type" and somehow logically tell which one is biggest
+}
