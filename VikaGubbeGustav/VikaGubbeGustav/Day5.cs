dccd62f4269cf134d87a78352eff7e1a0135c34b
Märta Norman <marta.norman@toteme.com>
2024-12-02 15:30:49 +0100

    Märta

--- VikaGubbeGustav/VikaGubbeGustav/Day5.cs
+++ VikaGubbeGustav/VikaGubbeGustav/Day5.cs
@@ -5,12 +5,28 @@ using System.Linq;
 
 public class Day5
 {
-    public static void Main(string[] args)
-    {
-        string[] lines = File.ReadAllLines("input.txt");
+  
 
+    // Seed 79 corresponds to soil 81
+    public static void SeedToSoil(string[] lines)
+    {
+        
         string[] seedStrings = lines[0].Split(": ")[1].Split(' ');
         List<long> seeds = seedStrings.Select(long.Parse).ToList();
+        List<long> soil = new List<long>();
+        
+        // Seed to Seed-to-Soil Map
+        var start = 50;
+        var SourceTarget = 98; // corresponds to soil number 50
+        var rangeLength = 2;
+        
+
+    }
+    
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines("input.txt");
+        SeedToSoil(lines);
     }
 
     //Sorry i didnt understand the task ¯\_( :( )_/¯
