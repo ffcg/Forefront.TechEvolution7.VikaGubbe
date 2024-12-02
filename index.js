b1ffec47020d1caaaab112acfa507a4752dbcd81
zeuph <kimmie.arvidsson@forefront.se>
2024-12-02 15:45:09 +0100

    changed monkey attributes

--- index.js
+++ index.js
@@ -10,44 +10,72 @@ function createMonkey(startingItems, operation, test, ifTrue, ifFalse) {
 //Get data from txt file
 const fs = require('fs');
 fs.readFile('monkeys.txt', 'utf8', (err, data) => {
-  if(err) {
+  if (err) {
     console.error("Failed to read the file:", err);
     return;
   }
 })
 
 const monkey0 = createMonkey(
-  [79, 98],
-  (old) => old * 19,
-  (newItem) => newItem % 23 === 0,
-  2,
-  3
+  [74, 73, 57, 77, 74],
+  (old) => old * 11,
+  (newItem) => newItem % 19 === 0,
+  6,
+  7
 );
 
 const monkey1 = createMonkey(
-  [54, 65, 75, 74],
-  (old) => old + 6,
+  [99, 77, 79],
+  (old) => old + 8,
   (newItem) => newItem % 19 === 0,
   2,
   0
 );
 
 const monkey2 = createMonkey(
-  [79, 60, 97],
-  (old) => old * old,
+  [64, 67, 50, 96, 89, 82, 82],
+  (old) => old + 1,
   (newItem) => newItem % 13 === 0,
   1,
   3
 );
 
 const monkey3 = createMonkey(
-  [74],
-  (old) => old + 3,
+  [88],
+  (old) => old + 7,
   (newItem) => newItem % 17 === 0,
   0,
   1
 );
-const monkeys = [monkey0, monkey1, monkey2, monkey3];
+const monkey4 = createMonkey(
+  [80, 66, 98, 83, 70, 63, 57, 66],
+  (old) => old + 4,
+  (newItem) => newItem % 17 === 0,
+  0,
+  1
+);
+const monkey5 = createMonkey(
+  [81, 93, 90, 61, 62, 64],
+  (old) => old + 7,
+  (newItem) => newItem % 17 === 0,
+  0,
+  1
+);
+const monkey6 = createMonkey(
+  [69, 97, 88, 93],
+  (old) => old * old,
+  (newItem) => newItem % 17 === 0,
+  0,
+  1
+);
+const monkey7 = createMonkey(
+  [59, 80],
+  (old) => old + 6,
+  (newItem) => newItem % 17 === 0,
+  0,
+  1
+);
+const monkeys = [monkey0, monkey1, monkey2, monkey3, monkey4, monkey5, monkey6, monkey7];
 // Started to simulate rounds to test
 for (let round = 0; round < 20; round++) {
   for (const monkey of monkeys) {
